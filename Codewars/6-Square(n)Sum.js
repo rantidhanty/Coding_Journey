ed57d001ed94fe821a4b3c315bc64b2be51416c9
Rantidhanty <rantidhanty@gmail.com>
2025-01-01 15:25:24 +0700

    function square sum

--- Codewars/6-Square(n)Sum.js
+++ Codewars/6-Square(n)Sum.js
@@ -7,15 +7,13 @@ https://www.codewars.com/kata/515e271a311df0350d00000f/solutions/javascript
 
 //cara 1
 function squareSum(numbers) {
-    let jumlah = 0;
-    for(let i = 0; i < numbers.length; i++){
-        jumlah += numbers[i] * numbers[i];
-    }
-    return jumlah
+  let jumlah = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    jumlah += numbers[i] * numbers[i];
+  }
+  return jumlah;
 }
-console.log(squareSum([1,2,2]))
-
-
+console.log(squareSum([1, 2, 2]));
 
 //cara 2
 // function squareSum(numbers){
