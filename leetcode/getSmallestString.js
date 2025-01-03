8dc808d1387e728cc4fcf9510760b1028eb0596a
Rantidhanty <rantidhanty@gmail.com>
2025-01-03 23:20:29 +0700

    function longValidParentheses

---
+++ leetcode/getSmallestString.js
@@ -0,0 +1,13 @@
+var getSmallestString = function (n, k) {
+  k -= n;
+  let alpha = "_bcdefghijklmnopqrstuvwxy_";
+  console.log(alpha);
+  let answer = "z".repeat(~~(k / 25));
+  console.log(answer);
+  if (k % 25) {
+    answer = alpha.charAt(k % 25) + answer;
+  }
+  return answer.padStart(n, "a");
+};
+
+console.log(getSmallestString(1, 5));
